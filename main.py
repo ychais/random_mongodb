6e36091a89e937b5cb435de0e718198a37fb406b
ychais <62468360+ychais@users.noreply.github.com>
2021-04-28 00:58:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 
+from faker import Faker
+fake = Faker()
+from 
+skills = ['Java', 'JavaScript', 'Python', 'SQL', 'C', 'C++', 'C#', 'Ruby', 'Docker', 'Grafana', 'Vue.js', 'Git', 'Angular', 'Erlang', 'Matlab', 'AutoCAD', 'PHP', 'Swift']
+
+
+for i in range(50):
+    print(fake.name())
+
+for i in range(50):
+    print(fake.phone_number())
+
+for i in range(50):
+    print(fake.numerify(text = '%#'))
+
+for i in range(50):   
+    a = fake.sentence(ext_word_list=skills)
+    print(a)
