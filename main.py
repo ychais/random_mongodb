9b417865bc08b8b2fe8917c268b16823eb2caf5a
ychais <62468360+ychais@users.noreply.github.com>
2021-04-28 15:41:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,52 @@ db = client.mt
 skills = ['Java', 'JavaScript', 'Python', 'SQL', 'C', 'C++', 'C#', 'Ruby', 'Docker', 'Grafana', 'Vue.js', 'Git', 'Angular', 'Erlang', 'Matlab', 'AutoCAD', 'PHP', 'Swift']
 
 for i in range(50):
+    id = fake.numerify(text = '###')
     username = fake.name()
-    age = fake.numerify(text = '%#')
+    age = fake.random_int(min = 20, max = 60)
     phone_number = fake.phone_number()
-    user_skills = fake.random_choices(elements=skills, length = 1)
+    user_skills = fake.random_choices(elements = skills, length = 1)
+    iban = fake.iban()
+    swift = fake.swift()
+    salary = fake.pyint()
+
+
+    db.salaries.insert_one({
+        "id": id,
+        "name": username,
+        "phone_number": phone_number,
+        "iban": iban,
+        "swift": swift,
+        "salary": salary,
+    })
+
     db.employees.insert_one({
+        "id": id,
         "name": username,
         "age": age,
         "phone_number": phone_number,
         "user_skills": user_skills
     })
+
+for i in range(120):
+    id = fake.numerify(text = '###')
+    username = fake.name()
+    age = fake.random_int(min = 20, max = 60)
+    phone_number = fake.phone_number()
+    job = fake.job()
+    address = fake.address()
+    iban = fake.iban()
+    swift = fake.swift()
+    email = fake.ascii_email()
+    db.clients.insert_one({
+        "id": id,
+        "name": username,
+        "age": age,
+        "phone_number": phone_number,
+        "job": job,
+        "address": address,
+        "iban": iban,
+        "swift": swift,
+        "email": email  
+    })
+
